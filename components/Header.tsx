dc8b51bdfd035a4368b60893fc3365079e187bc8
C1281964 Werick Silva Rodrigues Cruz <wericksilva-@hotmail.com>
2026-02-20 22:26:24 -0300

    TESTE DEPLOY VERCEL

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 export default function Header() {
   return (
     <header className="h-16 bg-white border-b flex items-center justify-between px-6">
+
      <h2 className="font-semibold text-lg ml-[59px]">
-  Painel da Clínica
-</h2>
+      Painel da Clínica
+    </h2>
+
       <div className="text-sm text-gray-600">
         admin@clinica.com
       </div>
